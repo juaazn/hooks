6ab90ee97bef2c8a76e28b8f7c5890e4d11aa62d
chuans <juaazn.05@gmail.com>
2024-07-25 13:12:49 +0200

    menu passed as props

--- restaurant/src/components/Card.jsx
+++ restaurant/src/components/Card.jsx
@@ -1,29 +1,8 @@
-export default function Card () {
-  const dishes = [
-    {
-      id:1,
-      name:'plato 1',
-      description:'Este es el plato 1',
-      price:10
-    },
-    {
-      id:2,
-      name:'plato 2',
-      description:'Este es el plato 2',
-      price:20
-    },
-    {
-      id:3,
-      name:'plato 3',
-      description:'Este es el plato 3',
-      price:15
-    }
-  ]  
-
+export default function Card (props) {
   return (
     <article className='flex_card' >
       {
-        dishes.map((item) => {
+        props.menu.map((item) => {
           return (
             <section className='card' key={item.id}>
               <h2>{item.name}</h2>
--- restaurant/src/components/Home.jsx
+++ restaurant/src/components/Home.jsx
@@ -1,10 +1,31 @@
 import Card from './Card'
 
 export default function Home () {
+  const dishes = [
+    {
+      id:1,
+      name:'plato 1',
+      description:'Este es el plato 1',
+      price:10
+    },
+    {
+      id:2,
+      name:'plato 2',
+      description:'Este es el plato 2',
+      price:20
+    },
+    {
+      id:3,
+      name:'plato 3',
+      description:'Este es el plato 3',
+      price:15
+    }
+  ]  
+
   return (
     <>
       <main className='container_card'>
-        <Card />
+        <Card menu={dishes}/>
       </main>
     </>
   )
